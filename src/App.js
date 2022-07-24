6302c6d5093d6b4b96dd5cafef3afed302564641
Saqib Hussain - Designer + Coder <saqibhussaincg@gmail.com>
2022-07-24 20:50:15 +0500

    'header'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Experience from './components/experience/Experience';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import Portfolio from './components/porfolio/Portfolio';
+import Service from './components/services/Service';
+import Testimonial from './components/testimonials/Testimonial';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Header />
+     <Nav />
+     <About />
+     <Experience />
+     <Service />
+     <Portfolio />
+     <Testimonial />
+     <Contact />
+     <Footer />
+    </>
   );
 }
 
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import CTA from './CTA';
+import './header.css';
+import ME from '../../assets/me.png'
+import HeaderSocial from './HeaderSocial';
+
+
+const Header = () => {
+  return (
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm</h5>
+        <h1>Saqib Hussain</h1>
+        <h5 className='text-light'>Front End Developer</h5>
+        <CTA />
+        <HeaderSocial />
+        <div className='me'>
+          <img src={ME} alt="foto" />
+        </div>
+
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
+  ) 
+}
+
+export default Header
