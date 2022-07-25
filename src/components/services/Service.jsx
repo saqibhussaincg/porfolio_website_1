8cde9da050a9ff8d243e45a54ecc5b680c119ea9
Saqib Hussain - Designer + Coder <saqibhussaincg@gmail.com>
2022-07-25 22:57:04 +0500

    services

--- src/components/services/Service.jsx
+++ src/components/services/Service.jsx
@@ -1,11 +1,125 @@
 import React from 'react'
 import './service.css';
+import {BsCheck} from 'react-icons/bs'
 
 
 const Service = () => {
   return (
     <section id='service'>
-     
+     <h5>What I Offer</h5>
+     <h2>Services</h2>
+
+     <div className='container services_container'>
+      <article className='service'>
+        <div className='service__head'>
+          <h3>UI/UX Design</h3>
+        </div>
+
+        <ul className='service__list'>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+        </ul>
+      </article>
+      {/* END OF UI/UX Design */}
+
+      <article className='service'>
+        <div className='service__head'>
+          <h3>Web Development</h3>
+        </div>
+
+        <ul className='service__list'>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+        </ul>
+      </article>
+      {/* END OF Web Development */}
+
+      <article className='service'>
+        <div className='service__head'>
+          <h3>Content Creation</h3>
+        </div>
+
+        <ul className='service__list'>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+          <li>
+            <BsCheck className='service__list-icon' />
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </li>
+        </ul>
+      </article>
+      {/* END OF Content Creation*/}
+     </div>
     </section>
   )
 }
